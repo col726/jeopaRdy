dcbb1f75913f2471fe9f054c11c564ec04e0514b
ColinM <col726@gmail.com>
2018-03-03 18:13:08 -0500

    refactored clues to their own function

--- jarchive.R
+++ jarchive.R
@@ -1,100 +1,44 @@
-library("rvest", lib.loc="~/R/win-library/3.4")
+source("clues.R")
 
-episodeID <- 5870
+x <- create_clues(5870)
 
-jarchive_page <- read_html(paste("http://www.j-archive.com/showgame.php?game_id=",episodeID, sep=''))
-jarchive_page_responses <- read_html(paste("http://www.j-archive.com/showgameresponses.php?game_id=",episodeID, sep=''))
-jarchive_page_scores <- read_html(paste("http://www.j-archive.com/showscores.php?game_id=", episodeID, sep=''))
+View(x)
 
-clue_values <- jarchive_page %>%
-  html_nodes(".clue_value_daily_double, .clue_value") %>%
-  html_text()
 
-if(length(clue_values) > 0)
-{
-  clue_values[length(clue_values) + 1] <- "n/a"
-  
-  clues <- jarchive_page %>%
-     html_nodes(".clue_text") %>%
-     html_text()
-  
-  categories <- jarchive_page %>%
-    html_nodes(".category_name") %>%
-    html_text()
-  
-  clue_order_number_j <- jarchive_page %>%
-     html_nodes("#jeopardy_round .clue_order_number") %>%
-     html_text() %>%
-     as.numeric()
-  
-  clue_order_number_dj <- jarchive_page %>%
-    html_nodes("#double_jeopardy_round .clue_order_number") %>%
-    html_text() %>%
-    as.numeric()
-  
-  
-  clue_order_number_dj <- clue_order_number_dj + length(clue_order_number_j)
-  
-  clue_order_number <- c(clue_order_number_j, clue_order_number_dj)
-  
-  clue_order_number[length(clue_order_number) + 1] <- length(clue_order_number) + 1
-  
-  clue_answers <- jarchive_page_responses %>%
-    html_nodes(".correct_response") %>%
-    html_text()
-  
-  scores <- jarchive_page_scores %>%
-    html_nodes("#jeopardy_round .scores_table") %>%
-    html_table(header = TRUE)
-  
-  scores_dj <- jarchive_page_scores %>%
-    html_nodes("#double_jeopardy_round .scores_table") %>%
-    html_table(header = TRUE, fill = TRUE)
-  
-  scores_fj <- jarchive_page_scores %>%
-    html_nodes("#final_jeopardy_round table") %>%
-    html_table(header = TRUE)
-  
-  
-  scores <- as.data.frame(scores)
-  colnames(scores)[1] <- "clue_order_number"
-  colnames(scores)[5] <- "other"
-  
-  scores_dj <- as.data.frame(scores_dj)
-  colnames(scores_dj)[1] <- "clue_order_number"
-  colnames(scores_dj)[5] <- "other"
-  
-  scores_fj <- as.data.frame(scores_fj[1])
-  scores_fj <- scores_fj[1,]
-  scores_fj["clue_order_number"] <- length(clue_order_number)
-  scores_fj["other"] <- NA
-  
-  scores_dj$clue_order_number <- scores_dj$clue_order_number + length(clue_order_number_j)
-  
-  scores_df <- rbind(scores, scores_dj, scores_fj)
-    
-  clues_df<-cbind.data.frame(clue_values, clues, clue_answers, clue_order_number)
-  
-  clues_df["round"] <- NA
-  
-  clues_df$round[clue_order_number <= length(clue_order_number_j)] <- "Jeopardy"
-  clues_df$round[clue_order_number > length(clue_order_number_j)] <- "Double Jeopardy"
-  clues_df$round[clue_order_number == length(clue_order_number)] <- "Final Jeopardy"
-  
-  clues_df["category"] <- NA
-  
-  categories_j <- categories[1:6]
-  categories_dj <- categories[7:12]
-  categories_fj <- categories[13]
-  
-  clues_df$category[j_df$round=="Jeopardy"]<- categories_j
-  clues_df$category[j_df$round=="Double Jeopardy"]<- categories_dj
-  clues_df$category[j_df$round=="Final Jeopardy"]<- categories_fj
-  
-  clues_df["jid"] <-(episodeID * 100) + clues_df["clue_order_number"]
-  
-  j_df <- merge(clues_df, scores_df, by="clue_order_number")
-  
-  write.csv(j_df, "j_archive.csv")
-  View(j_df)
-}
+# jarchive_page_scores <- read_html(paste("http://www.j-archive.com/showscores.php?game_id=", episodeID, sep=''))
+# 
+#   scores <- jarchive_page_scores %>%
+#     html_nodes("#jeopardy_round .scores_table") %>%
+#     html_table(header = TRUE)
+#   
+#   scores_dj <- jarchive_page_scores %>%
+#     html_nodes("#double_jeopardy_round .scores_table") %>%
+#     html_table(header = TRUE, fill = TRUE)
+#   
+#   scores_fj <- jarchive_page_scores %>%
+#     html_nodes("#final_jeopardy_round table") %>%
+#     html_table(header = TRUE)
+#   
+#   
+#   scores <- as.data.frame(scores)
+#   colnames(scores)[1] <- "clue_order_number"
+#   colnames(scores)[5] <- "other"
+#   
+#   scores_dj <- as.data.frame(scores_dj)
+#   colnames(scores_dj)[1] <- "clue_order_number"
+#   colnames(scores_dj)[5] <- "other"
+#   
+#   scores_fj <- as.data.frame(scores_fj[1])
+#   scores_fj <- scores_fj[1,]
+#   scores_fj["clue_order_number"] <- length(clue_order_number)
+#   scores_fj["other"] <- NA
+#   
+#   scores_dj$clue_order_number <- scores_dj$clue_order_number + length(clue_order_number_j)
+#   
+#   scores_df <- rbind(scores, scores_dj, scores_fj)
+#     
+#   j_df <- merge(clues_df, scores_df, by="clue_order_number")
+#   
+#   write.csv(j_df, "j_archive.csv")
+#   View(j_df)
+# }
