28e0574c27d9a396f704bc3b4812d37793df110a
ColinM <col726@gmail.com>
2019-04-06 11:14:15 -0400

    Updated to handle tiebreaker episodes

--- clues.R
+++ clues.R
@@ -5,13 +5,27 @@ create_clues <- function(episodeID){
   jarchive_page <- read_html(paste("http://www.j-archive.com/showgame.php?game_id=",episodeID, sep=''))
   jarchive_page_responses <- read_html(paste("http://www.j-archive.com/showgameresponses.php?game_id=",episodeID, sep=''))
   
+  tiebreaker <- FALSE
+  
   clue_values <- jarchive_page %>%
     html_nodes(".clue_value_daily_double, .clue_value") %>%
     html_text()
   
   if(length(clue_values) > 0)
   {
-    clue_values[length(clue_values) + 1] <- "n/a"
+    categories <- jarchive_page %>%
+      html_nodes(".category_name") %>%
+      html_text()
+    
+    if(length(categories) == 14)
+    {
+      tiebreaker <- TRUE
+      clue_values[length(clue_values) + 1] <- "n/a"
+      clue_values[length(clue_values) + 1] <- "n/a"
+    }
+    else
+      clue_values[length(clue_values) + 1] <- "n/a"
+    
     
     clues <- jarchive_page %>%
       html_nodes(".clue_text") %>%
@@ -36,7 +50,15 @@ create_clues <- function(episodeID){
     
     clue_order_number <- c(clue_order_number_j, clue_order_number_dj)
     
-    clue_order_number[length(clue_order_number) + 1] <- length(clue_order_number) + 1
+    if(tiebreaker)
+    {
+      clue_order_number[length(clue_order_number) + 1] <- length(clue_order_number) + 1
+      clue_order_number[length(clue_order_number) + 1] <- length(clue_order_number) + 1
+    }
+    else
+    {
+      clue_order_number[length(clue_order_number) + 1] <- length(clue_order_number) + 1
+    }
     
     clue_answers <- jarchive_page_responses %>%
       html_nodes(".correct_response") %>%
@@ -48,17 +70,43 @@ create_clues <- function(episodeID){
     
     clues_df$round[clue_order_number <= length(clue_order_number_j)] <- "Jeopardy"
     clues_df$round[clue_order_number > length(clue_order_number_j)] <- "Double Jeopardy"
-    clues_df$round[clue_order_number == length(clue_order_number)] <- "Final Jeopardy"
+    if(tiebreaker)
+    {
+      clues_df$round[clue_order_number == length(clue_order_number) - 1] <- "Final Jeopardy"
+      clues_df$round[clue_order_number == length(clue_order_number)] <- "Tiebreaker"
+    }
+    else
+    {
+      clues_df$round[clue_order_number == length(clue_order_number)] <- "Final Jeopardy"
+    }
     
     clues_df["category"] <- NA
     
     categories_j <- categories[1:6]
     categories_dj <- categories[7:12]
-    categories_fj <- categories[13]
+    if(tiebreaker)
+    {
+      categories_fj <- categories[13]
+      categories_tb <- categories[14]
+    }
+    else
+    {
+      categories_fj <- categories[13]
+    }
+    
     
     clues_df$category[clues_df$round=="Jeopardy"]<- categories_j
     clues_df$category[clues_df$round=="Double Jeopardy"]<- categories_dj
-    clues_df$category[clues_df$round=="Final Jeopardy"]<- categories_fj
+    if(tiebreaker)
+    {
+      clues_df$category[clues_df$round=="Final Jeopardy"]<- categories_fj
+      clues_df$category[clues_df$round=="Tiebreaker"]<- categories_tb
+    }
+    else
+    {
+      clues_df$category[clues_df$round=="Final Jeopardy"]<- categories_fj
+    }
+    
     
     clues_df["jid"] <-(episodeID * 100) + clues_df["clue_order_number"]
     
@@ -67,6 +115,3 @@ create_clues <- function(episodeID){
   
   return(NULL)
 }
-
-
-
