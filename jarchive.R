5da061801e834445a59b7da60b55abe139f55003
ColinM <col726@gmail.com>
2019-04-06 08:24:10 -0400

    Cleaned up code, added better logging, default loads all episodes as of 4/6/2019

--- jarchive.R
+++ jarchive.R
@@ -1,60 +1,29 @@
 source("clues.R")
 
-final <- create_clues(5870)
+final <- create_clues(173)
 
-for(id in 5800:5850){
+startEp <- 174
+endEp <- 6257
+
+totalEp <- endEp - startEp
+
+#6257 most recent episode
+
+for(id in startEp:endEp){
   
-  x <- create_clues(id)
+  x <- suppressWarnings(create_clues(id))
   
   if(!is.null(x))
   {
     final <-rbind(final, x)
-    sprintf("Episode %s added to data set.", id)
+    cat(sprintf("Episode %s added to data set.\n", id))
   }
   else{
-    sprintf("Skipping episode %s. No data returned.", id)
+    cat(sprintf("Skipping episode %s. No data returned.\n", id))
   }
+  prog <- round(((id - startEp)/totalEp) * 100, 1)
+  cat(sprintf("Processed %g%% of all episodes.\n", prog))
 }
 
-
-
 View(final)
-
-
-# jarchive_page_scores <- read_html(paste("http://www.j-archive.com/showscores.php?game_id=", episodeID, sep=''))
-# 
-#   scores <- jarchive_page_scores %>%
-#     html_nodes("#jeopardy_round .scores_table") %>%
-#     html_table(header = TRUE)
-#   
-#   scores_dj <- jarchive_page_scores %>%
-#     html_nodes("#double_jeopardy_round .scores_table") %>%
-#     html_table(header = TRUE, fill = TRUE)
-#   
-#   scores_fj <- jarchive_page_scores %>%
-#     html_nodes("#final_jeopardy_round table") %>%
-#     html_table(header = TRUE)
-#   
-#   
-#   scores <- as.data.frame(scores)
-#   colnames(scores)[1] <- "clue_order_number"
-#   colnames(scores)[5] <- "other"
-#   
-#   scores_dj <- as.data.frame(scores_dj)
-#   colnames(scores_dj)[1] <- "clue_order_number"
-#   colnames(scores_dj)[5] <- "other"
-#   
-#   scores_fj <- as.data.frame(scores_fj[1])
-#   scores_fj <- scores_fj[1,]
-#   scores_fj["clue_order_number"] <- length(clue_order_number)
-#   scores_fj["other"] <- NA
-#   
-#   scores_dj$clue_order_number <- scores_dj$clue_order_number + length(clue_order_number_j)
-#   
-#   scores_df <- rbind(scores, scores_dj, scores_fj)
-#     
-#   j_df <- merge(clues_df, scores_df, by="clue_order_number")
-#   
-#   write.csv(j_df, "j_archive.csv")
-#   View(j_df)
-# }
+write.csv(final, file="jeopardy.csv")
